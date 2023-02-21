7c10379a4a69f2b22b9ca262bbc25636c6509985
Nathan King <nathanking3590@gmail.com>
2023-02-21 14:26:44 +0000

    update package

--- src/test/java/com/booleanuk/core/ExerciseTest.java
+++ src/test/java/com/booleanuk/core/ExerciseTest.java
@@ -1,12 +1,47 @@
 package com.booleanuk.core;
 
-import com.booleanuk.core.Exercise;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ExerciseTest {
+    Exercise exercise;
+
+    public ExerciseTest() {
+        this.exercise = new Exercise();
+    }
+
     @Test
-    public void shouldSayHelloWorld() {
-        Assertions.assertEquals("Hello, world!", Exercise.sayHelloWorld());
+    public void shouldAnswerTheUltimateQuestion() {
+        Assertions.assertEquals(42, this.exercise.answerToTheUltimateQuestion);
+    }
+
+    @Test
+    public void shouldBeAged32() {
+        Assertions.assertEquals(32, this.exercise.age);
+    }
+
+    @Test
+    public void shouldBeNamedJane() {
+        Assertions.assertEquals("Jane", this.exercise.firstName);
+    }
+
+    @Test
+    public void shouldBeAProgrammer() {
+        Assertions.assertTrue(this.exercise.isProgrammer);
+    }
+
+    @Test
+    public void firstNumberShouldBe9182() {
+        Assertions.assertEquals(9182, this.exercise.firstNumber);
+    }
+
+    @Test
+    public void firstStringShouldBeSpecific() {
+        Assertions.assertEquals("Java is to Javascript what car is to carpet.", this.exercise.firstString);
+    }
+
+    @Test
+    public void isVisibleShouldBePublic() {
+        Assertions.assertTrue(this.exercise.isVisible);
     }
 }
