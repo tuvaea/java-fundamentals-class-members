a2d32ac3495386638b10d32aed19f055026de29a
Dave Ames <david.john.ames@gmail.com>
2023-03-09 11:58:45 +0000

    Added the functionality to make the tests run and display in the order they are defined.

--- src/test/java/com/booleanuk/core/ExerciseTest.java
+++ src/test/java/com/booleanuk/core/ExerciseTest.java
@@ -2,9 +2,13 @@ package com.booleanuk.core;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.lang.reflect.Field;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ExerciseTest {
     Exercise exercise;
     Field[] fields;
@@ -15,36 +19,43 @@ class ExerciseTest {
     }
 
     @Test
+    @Order(1)
     public void shouldAnswerTheUltimateQuestion() {
         Assertions.assertEquals(42, this.exercise.answerToTheUltimateQuestion);
     }
 
     @Test
+    @Order(2)
     public void shouldBeAged32() {
         Assertions.assertEquals(32, this.exercise.age);
     }
 
     @Test
+    @Order(3)
     public void shouldBeNamedJane() {
         Assertions.assertEquals("Jane", this.exercise.firstName);
     }
 
     @Test
+    @Order(4)
     public void shouldBeAProgrammer() {
         Assertions.assertTrue(this.exercise.isProgrammer);
     }
 
     @Test
+    @Order(5)
     public void firstNumberShouldBe9182() {
         Assertions.assertEquals(9182, this.exercise.firstNumber);
     }
 
     @Test
+    @Order(6)
     public void firstStringShouldBeSpecific() {
         Assertions.assertEquals("Java is to Javascript what car is to carpet.", this.exercise.firstString);
     }
 
     @Test
+    @Order(7)
     public void isVisibleShouldBePublic() {
         // Loop through all of the available fields in the Exercise class
         for (Field f : fields) {
